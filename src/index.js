d30bf1d7aa4646736ee1c03a2d5fc54388e5cc01
Daisuke Matsumoto <78680065+mazmoto14@users.noreply.github.com>
2021-02-23 20:58:50 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import "./styles.css";
+/**
+ * const, let等の変数宣言
+ */
 
-console.log("aaa");
+// var val1 = 'var変数';
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = 'var変数上書き';
+// console.log(val1);
+
+// var val1 = 'var変数再宣言';
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// val2 = 'let変数を上書き';
+// console.log(val2)
+
+// let val2 = 'let変数を再宣言';
+
+// const val3 = 'const変数';
+// console.log(val3);
+
+// val3 = 'const変数を上書き';
+
+// const val3 = 'const変数を再宣言';
+
+// const val4 = {
+//   name: 'じゃけえ',
+//   age: 28,
+// }
+// console.log(val4);
+// val4.name = 'jak';
+
+// val4.adress = 'Nara';
+// console.log(val4);
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = 'bird';
+// val5.push('monkey');
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = 'じゃけぇ';
+// const age = 28;
+
+// // const message1 = '私の名前は'　+ name + 'です。年齢は' + age + 'です。';
+// // console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
